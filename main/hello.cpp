fb6872f1d2224e522f3c5dee5629b6dd78fc40a8
anirudhmv9 <anirudhmv339@gmail.com>
2025-03-18 19:19:30 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I - Rolex :) have successfully built and run this  \n";
+  cout << "I - Rolex :) have successfully built and run this JUNKins  \n";
   return 0;
 }
